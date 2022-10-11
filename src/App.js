34cd8d371510fa5a279df7d96b685b4ea0047b40
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-11 15:25:01 +0300

    sass

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./scss/App.scss";
-
+import data from "./data";
 function App() {
   return (
     <>
       <Header />
-      <Card />
+      <Card data={data} />
       <Footer />
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,25 @@
-const Card = () => {
-  return <h1>Card</h1>;
+import CardStyle from "../scss/card.module.scss";
+
+const Card = ({ data }) => {
+  return (
+    <div className={CardStyle.container}>
+      {data.map((item) => {
+        const { id, name, job, comment, img } = item;
+        return (
+          <div key={id}>
+            <h1>{name}</h1>
+            <h2>{job}</h2>
+            <p>{comment}</p>
+            <img src={img} alt="img" />
+            <div>
+              <button>Small</button>
+              <button>Large</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
